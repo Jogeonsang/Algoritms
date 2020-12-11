14563bcd0b2b45e3daa76562648048758fa377be
marcus <rhcn258@gmail.com>
2020-12-11 11:34:46 +0900

    Add a Abstract Data Type
    Queue, Stack

---
+++ Abstract Data Type/queue.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+class Queue(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return not bool(self.items)
+
+    def enqueue(self, value):
+        return self.items.insert(0, value)
+
+    def dequeue(self):
+        value = self.items.pop()
+        if value is not None:
+            return value
+        else:
+            print("Queue is Empty")
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        else:
+            print("Queue is Empty")
+
+    def size(self):
+        return len(self.items)
+
+    def __repr__(self):
+        return repr(self.items)
+
+if __name__ == "__main__":
+    queue = Queue()
+    print("큐가 비었나요? {0}".format(queue.isEmpty()))
+    print("큐에 숫자 0~9를 추가합니다.")
+    for i in range(10):
+        queue.enqueue(i)
+    print("큐 크기: {0}".format(queue.size()))
+    print("peek: {0}".format(queue.peek()))
+    print("dequeue: {0}".format(queue.dequeue()))
+    print("peek: {0}".format(queue.peek()))
+    print("큐가 비었나요? {0}".format(queue.isEmpty()))
+    print(queue)
